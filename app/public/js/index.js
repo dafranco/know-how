40519c2d9381219a528c2ad0ebdbadf13a1c7433
David Franco <francodavid20@gmail.com>
2018-01-13 21:04:56 -0300

    Optimize js loading

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,20 +1,22 @@
-$(document).ready(function () {
-  $('#auditorias-link').click(function (evt) {
-    $('#auditoriasModal').modal();
+window.onload = function () {
+  $(window).ready(function () {
+    $('#auditorias-link').click(function (evt) {
+      $('#auditoriasModal').modal();
+    })
+    $('#capacitaciones-link').click(function () {
+      $('#capacitacionesModal').modal();
+    })
+    $('#liquidaciones-link').click(function () {
+      $('#liquidacionesModal').modal();
+    })
+    $('#manuales-link').click(function () {
+      $('#manualesModal').modal();
+    })
+    $('#eventos-link').click(function () {
+      $('#eventosModal').modal();
+    })
+    $('#reclutamiento-link').click(function () {
+      $('#reclutamientoModal').modal();
+    })
   })
-  $('#capacitaciones-link').click(function(){
-    $('#capacitacionesModal').modal();
-  })
-  $('#liquidaciones-link').click(function(){
-    $('#liquidacionesModal').modal();
-  })
-  $('#manuales-link').click(function(){
-    $('#manualesModal').modal();
-  })
-  $('#eventos-link').click(function(){
-    $('#eventosModal').modal();
-  })
-  $('#reclutamiento-link').click(function(){
-    $('#reclutamientoModal').modal();
-  })
-})
+}
