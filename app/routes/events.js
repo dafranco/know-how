be393172186cd7594154ed00b0aa64909f904e16
David Franco <francodavid20@gmail.com>
2018-02-07 08:56:29 -0300

    Redirect on events creation

--- app/routes/events.js
+++ app/routes/events.js
@@ -28,7 +28,7 @@ router.post('/', upload.single('image'), function (req, res, next) {
     description: req.body.description
   })
   return created_event.save()
-    .then(created => res.status(200).json(created))
+    .then(created => res.redirect('/admin/events'))
     .catch(err => res.status(500).json(err));
 })
 
